28dded11fda2c6d1d0dbc04ca1c651147ab64df0
unknown <cskiera142@gmail.com>
2023-11-26 09:08:11 -0700

    breaking up into controllers and routes

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route not implemented yet",
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route not implemented yet",
+  });
+};
+const createUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route not implemented yet",
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route not implemented yet",
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "Route not implemented yet",
+  });
+};
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
 
+const userRouter = require("./routes/userRoutes");
+const tourRouter = require("./routes/toursRoutes");
+
 const app = express();
-app.use(express.json());
 
 // Middleware
-
+app.use(express.json());
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
@@ -18,143 +19,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// Routes
-
-const tours = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
-
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (id > tours.length) {
-    // if (!tour) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid Id",
-    });
-  }
-
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tour,
-    },
-  });
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid Id",
-    });
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour/>",
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid Id",
-    });
-  }
-  res.status(204).json({
-    status: "success",
-    data: {
-      data: null,
-    },
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Route not implemented yet",
-  });
-};
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Route not implemented yet",
-  });
-};
-const createUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Route not implemented yet",
-  });
-};
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Route not implemented yet",
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "Route not implemented yet",
-  });
-};
-
-// Route Handlers
-
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
-app
-  .route("/api/v1/tours/:id")
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-app.route("/api/v1/users").get(getAllUsers).post(createUsers);
-
-app
-  .route("/api/v1/users/:id")
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
 
 // Server
 
